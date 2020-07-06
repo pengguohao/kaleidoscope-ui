53c0114b36f640e62f858780cf24824b6f186f53
彭国豪 <peng_gh@tiza.com.cn>
2020-07-06 14:45:05 +0800

    1、前端项目初始化，基于spring-blade的saber

---
+++ src/api/user.js
@@ -0,0 +1,60 @@
+import request from '@/router/axios';
+import {baseUrl} from '@/config/env';
+import website from "@/config/website";
+
+export const loginByUsername = (tenantId, account, password, type, key, code) => request({
+  url: '/api/blade-auth/token',
+  method: 'post',
+  headers: {
+    'Captcha-Key': key,
+    'Captcha-Code': code,
+  },
+  params: {
+    grantType: (website.captchaMode ? "captcha" : "password"),
+    tenantId,
+    account,
+    password,
+    type
+  }
+});
+
+export const getButtons = () => request({
+  url: '/api/blade-system/menu/buttons',
+  method: 'get'
+});
+
+export const getUserInfo = () => request({
+  url: baseUrl + '/user/getUserInfo',
+  method: 'get'
+});
+
+export const refeshToken = () => request({
+  url: baseUrl + '/user/refesh',
+  method: 'post'
+})
+
+export const getMenu = () => request({
+  url: '/api/blade-system/menu/routes',
+  method: 'get'
+});
+
+export const getCaptcha = () => request({
+  url: '/api/blade-auth/captcha',
+  method: 'get'
+});
+
+export const getTopMenu = () => request({
+  url: baseUrl + '/user/getTopMenu',
+  method: 'get'
+});
+
+export const sendLogs = (list) => request({
+  url: baseUrl + '/user/logout',
+  method: 'post',
+  data: list
+})
+
+export const logout = () => request({
+  url: baseUrl + '/user/logout',
+  method: 'get'
+})
