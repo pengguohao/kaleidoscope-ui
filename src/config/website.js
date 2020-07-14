427c6bc77c1bc331fb4fb6a7bd8f0eeb30f4ab4c
彭国豪 <peng_gh@tiza.com.cn>
2020-07-14 09:02:28 +0800

    1、暂时关闭租户模式

--- src/config/website.js
+++ src/config/website.js
@@ -2,11 +2,11 @@
  * 全局配置文件
  */
 export default {
-  title: "saber",
+  title: "Kaleidoscope",
   indexTitle: 'Saber Admin',
   clientId: 'saber', // 客户端id
   clientSecret: 'saber_secret', // 客户端密钥
-  tenantMode: true, // 是否开启租户模式
+  tenantMode: false, // 是否开启租户模式
   captchaMode: true, // 是否开启验证码模式
   logo: "S",
   key: 'saber',//配置主键,目前用于存储
